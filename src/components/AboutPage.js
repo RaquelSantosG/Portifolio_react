3e6e4c63d05fecc514c23ce1a58cc2a1c21588c5
raquelnew <raquelnew3@gmail.com>
2022-01-23 16:16:29 -0300

    portifolio

---
+++ src/components/AboutPage.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/spaceman.png'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+
+`
+const Spaceman = styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+img{
+    width: 100%;
+    height: auto;
+}
+`
+const Main =  styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+ backdrop-filter: blur(4px);
+  
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
+`
+
+
+
+
+const AboutPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+<Box>
+
+<LogoComponent theme='dark'/>
+<SocialIcons theme='dark'/>
+<PowerButton />
+<ParticleComponent theme='dark' />
+
+        <Spaceman>
+            <img src={astronaut} alt="spaceman" />
+        </Spaceman>    
+        <Main>
+        Olá,meu nome é Raquel dos santos, tenho 30 anos,moro na zona leste-Sp.
+Desde pequena sempre fui muito curiosa e sempre gostei de estudar, ler e de ouvir musicas.comecei a trabalhar com 14 anos para ajudar em casa e conseguir investir nos meus estudos.No meu ultimo emprego foi onde surgiu o meu interesse na area de tecnologia e onde eu realmente consegui paga minha faculdade,nessa minha ultima experiência profissional foi em uma empresa de transporte
+onde de auxiliar de limpeza busquei conhecimento e me tornei a primeira lider mulher do setor de manutenção.Hoje estou no meu último ano do curso sistemas de informação.
+Sou muita comprometida com meus objetivos,tarefas e apaixonada por tecnologia.
+        </Main>
+
+        <BigTitle text="ABOUT" top="10%" left="5%" />
+
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default AboutPage
---
+++ src/data/BlogData.js
@@ -0,0 +1,30 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Meu primeiro Portifolio",
+        tags:[""],
+        imgSrc:"https://raquelnew.netlify.app/images/Fa%C3%A7a%20o%20Que%20%C3%89%20Certo%20_%20C%C3%A9u%20Estrelado%20Facebook%20Capa.png",
+        link:"https://raquelnew.netlify.app/"
+    },
+    {
+        id:2,
+        name:"Meu segundo Portifolio",
+        tags:["react","java"],
+        imgSrc:"https://i.imgur.com/4Ba5XBJ.png",
+        link:"https://raquelsantosg.github.io/BlogPessoal-Front-End/"
+    },
+    {
+        id:3,
+        name:"Y-Eco e-commerce",
+        tags:["react","java"],
+        date:"2021",
+        imgSrc:"https://i.imgur.com/xFvpWBr.gif",
+        link:"https://y-eco-e-commerce.herokuapp.com/"
+    },
+  
+   
+   
+  
+] 
